d0f0fbe57b53476ba8d76bbe6893a020fda790a9
shinlydia22 <143225542+shinlydia22@users.noreply.github.com>
2023-11-08 20:25:31 -0600

    basic shell front end

--- Project/streamlitTest.py
+++ Project/streamlitTest.py
@@ -3,4 +3,11 @@ import streamlit as st
 st.title('streamlit test')
 video_url = st.text_input('paste URL')
 
-if st.button()
+if st.button('start analysis'):
+    if video_url:
+        #analysis
+        st.text('yay u pasted a URL good job buds')
+        st.text(video_url)
+        #show outcome
+    else:
+        st.warning('u didnt paste a URL >:/')
